7a6a63a02b2b45863f5bbc83768c0a5155bb8231
Rahul Kambariya <rahulkambariya.000@gmail.com>
2025-09-08 16:29:13 +0530

    image chage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Miracle",
+  title: "Dev&Chill",
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
